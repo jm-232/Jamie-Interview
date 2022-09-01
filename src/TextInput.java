77cf129b71fb8f723830a4ddbf5d6fac394d923f
John McLellan <jm232@hw.ac.uk>
2022-09-01 16:35:22 +0100

    Added the a command

--- src/TextInput.java
+++ src/TextInput.java
@@ -11,21 +11,44 @@ public class TextInput {
 		storedStrings.add("I can’t wait for User Interfaces to be invented.");
 		storedStrings.add("Then I can do much more complicated things");
 		Scanner textRead = new Scanner(System.in);
+		String currentText = "";
 		
 		System.out.println("Welcome!");
 		System.out.println("I,l = Print All");
 		System.out.println("e = Exit");
-		do {
-			String input = textRead.next();
-			switch(input) {
-			case("I"):
-			case("L"):
+		
+		String input;
+		char command='I';
+		currentText="";
+		do{
+			//Takes and splits the input
+			input = textRead.next();
+			command=input.charAt(0);
+			currentText="";
+			if(input.length()>2) {
+				currentText=input.substring(2);
+			}
+			switch(command) {
+			case 'I':
+			case 'L':
 				//Print all Strings
 				for(int i=0;i<storedStrings.size();i++) {
 					System.out.println(storedStrings.get(i));
 				}
+				break;
+			case 'a':
+				//Adds the text after the command, if there is text
+				if(!currentText.equals("")) {
+					storedStrings.add(currentText);
+					System.out.println("Added "+currentText);
+				}else {
+					System.out.println("No text to add");
+				}
+					
+				break;
 			}
-		}while(!textRead.equals("e"));
+		}while(!(command=='e'));
+		System.out.println("exiting");
 		
 		textRead.close();
 	}
